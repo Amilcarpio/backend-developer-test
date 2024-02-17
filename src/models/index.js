ee333df0b250881de9ea2ed2e5f01549cf435c01
Amilcar Pio <amilcar.pio@gmail.com>
2024-02-17 16:13:43 -0300

    Fix: correções sequelize config.

--- src/models/index.js
+++ src/models/index.js
@@ -5,7 +5,7 @@ import fs from 'fs';
 
 const __dirname = path.dirname(fileURLToPath(import.meta.url));
 
-const configPath = path.resolve(__dirname, './config/config.json');
+const configPath = path.resolve(__dirname, '../../database/config/config.json');
 const configFile = fs.readFileSync(configPath, 'utf-8');
 const config = JSON.parse(configFile);
 
