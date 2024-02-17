7aeb0ded15c1a35897e02a02cd7005e149245feb
Amilcar Pio <amilcar.pio@gmail.com>
2024-02-17 16:14:56 -0300

    Feat: testes de integração API Company

--- test/integration/company.test.js
+++ test/integration/company.test.js
@@ -1,9 +1,16 @@
-import chai from 'chai';
-const expect = chai.expect()
+import axios from 'axios'
+import { expect, assert, should } from 'chai'
 
-describe('API integration test', () => {
-    it('Should test the API status', async () => {
-        const response = await chai.request('http://localhost:3000').get('/')
-        expect(response.status).to.equal(200)
+describe('GET /companies', () => {
+    it('Deve retornar uma lista com as empresas', async () => {
+        const res = await axios.get('http://localhost:3000/api/companies')
+        expect(res.status).to.equal(200)
+    })
+})
+
+describe('GET /companies/:company_id', () => {
+    it('Deve retornar uma empresa pelo seu id', async () => {
+        const res = await axios.get('http://localhost:3000/api/companies/1')
+        expect(res.status).to.equal(200)
     })
 })
