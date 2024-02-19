5feaad2793cea960058ae660ad34436d60dc9255
Amilcar Pio <amilcar.pio@gmail.com>
2024-02-19 18:22:46 -0300

    WIP: Validações API Jobs

--- routes/api.js
+++ routes/api.js
@@ -9,26 +9,76 @@ const companyController = new CompanyController();
 const router = express.Router();
 
 router.post('/job', async (req, res) => {
-    try {
+    try {   
         const jobDAO = {
             title: req.body.title,
             description: req.body.description,
             location: req.body.location,
             notes: req.body.notes,
-            status: req.body.status,
             companyId: req.body.companyId
         }
+
+        console.log('=============Recebido: ' + JSON.stringify(jobDAO))
+
+        const searchDAOCompany = await companyController.get(jobDAO.companyId)
+
+
+        if(!jobDAO.companyId || typeof jobDAO.companyId !== 'number'){
+            res.status(400).json({status: 'error', message: 'CompanyId is invalid'})
+            console.log('CompanyId is invalid')
+            return;
+        }
+
+        if(!searchDAOCompany){
+            res.status(404).json({status: 'error', message: 'CompanyId not found'})
+            console.log('CompanyId not found')
+            return;
+        }
+
+        if(!jobDAO.title || typeof jobDAO.title !== 'string'){
+            res.status(400).json({status: 'error', message: 'Title is invalid'})
+            console.log('Title is invalid')
+            return;
+        }
+
+        if(!jobDAO.description || typeof jobDAO.description !== 'string'){
+            res.status(400).json({status: 'error', message: 'Description is invalid'})
+            console.log('Description is invalid')
+            return;
+        }
+
+        if(!jobDAO.location || typeof jobDAO.location !== 'string'){
+            res.status(400).json({status: 'error', message: 'Location is invalid'})
+            console.log('Location is invalid')
+            return;
+        }
+
         const result = await jobController.save(jobDAO)
-        res.status(201).json(result)
+        res.status(201).json({status: 'created', result: result})
     } catch (error) {
-        res.status(500).json(error)
-        console.log('error' + error)
+        res.status(500).json({status: 'error', message: error.message})
+        console.log('Error at POST/job: ' + error)
     }
 })
 
 router.put('/job/:job_id/publish', async (req, res) => {
     try {
         const jobId = req.params.job_id
+        console.log('jobId: ' + jobId)
+        const searcJob = await jobController.get(jobId)
+        if(searcJob.status === 'published'){
+            res.status(409).json({status: 'error', message: 'Job already published'})
+            console.log('Job already published')
+            return;
+        }
+
+        if(!searcJob){
+            res.status(404).json({status: 'error', message: 'Job not found'})
+            console.log('Job not found')
+            return;
+        }
+
+
         const result = await jobController.publish(jobId)
         res.status(200).json({status: 'published', result: result})
     } catch (error) {
@@ -93,6 +143,14 @@ router.get('/companies/:company_id', async (req,res) => {
     } catch (error) {
         res.status(500).json(error)
         console.log('error' + error)
+    }
+})
+
+router.get('/feed', async (req,res) => {
+    try {
+        
+    } catch (error) {
+        
     }
 })
 
