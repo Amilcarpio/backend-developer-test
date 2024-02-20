35be4cd02973611570832aba73466b9fb107f409
Amilcar Pio <amilcar.pio@gmail.com>
2024-02-20 15:35:56 -0300

    Fix: Adjustments of sync db service

--- database/syncDatabase.js
+++ database/syncDatabase.js
@@ -2,14 +2,14 @@ import {sequelize} from '../src/models/sequelizeConfig.js'
 import seedData from './dataSeeder.js'
 async function syncDatabase(force) {
     try {
-        console.log('===========syncDatabase      #Iniciando')
+        console.log('===========syncDatabase      #Starting')
         if(force){
             await sequelize.sync({force: true})
         }else {
             await sequelize.sync({alter: true})
         }
         await seedData();
-        console.log('===========syncDatabase      #Dados sincronizados')
+        console.log('===========syncDatabase      #Data synced successfuly')
     } catch (error) {
         console.log('error'+error)
     }
