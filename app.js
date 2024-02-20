6655bdb24e0634717b7ca4c6c13c3b49a793abd6
Amilcar Pio <amilcar.pio@gmail.com>
2024-02-19 23:21:27 -0300

    Fix: Refactoring server

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import createError from 'http-errors'
 import cors from 'cors'
 import cookieParser from 'cookie-parser'
 import apiRouter from './routes/api.js'
+import syncDatabase from './database/syncDatabase.js'
 
 const app = express()
 
@@ -15,22 +16,25 @@ app.use(cookieParser())
 
 app.use('/api', apiRouter)
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
     next(createError(404));
   });
 
-// error handler
 app.use(function(err, req, res, next) {
-// set locals, only providing error in development
 res.locals.message = err.message;
 res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-// render the error page
 res.status(err.status || 500);
 res.render('error');
 });
 
-export default app
+syncDatabase().then(() => {
+    app.listen(3000, () => {
+        console.log('Server running on port 3000')
+    })
+}).catch((error) => {
+    console.log('error'+error)
+})
+
 
 
