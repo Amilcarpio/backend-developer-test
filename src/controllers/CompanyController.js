f795b86080e6d1d8f0339bb76a7715b42409f520
Amilcar Pio <amilcar.pio@gmail.com>
2024-02-19 23:20:42 -0300

    Fix: Company API Refactoring

--- src/controllers/CompanyController.js
+++ src/controllers/CompanyController.js
@@ -1,101 +1,30 @@
-import { Company } from '../models/sequelizeConfig.js'
-import { Job } from '../models/sequelizeConfig.js'
-class Companies {
-    async save(DAO) {
-      try {
-        console.log('===========CompanyController      #save')
-        const item = await Company.create({
-            name: DAO.name
-        })
-
-        return item
-      } catch (error) {
-        console.log('error' + error)
-        throw new Error(error)
-      }
+class CompanyController {
+    constructor (companyRepository) {
+        this.companyRepository = companyRepository
     }
 
-  async get (id) {
-    try {
-    console.log('===========CompanyController      #get')
-      let where = {}
-      where.id = id
-
-      const data = await Company.findOne({
-        attributes: ['name'],
-        where: where,
-        limit: 1,
-        offset: 0
-      })
-
-      return data
-    } catch (error) {
-      console.log('error' + error)
-      throw new Error(error)
+    async list(req,res){
+        try {
+            const companies = await this.companyRepository.list(null, null)
+            res.status(200).json(companies)
+        } catch (error) {
+            res.status(500).json(error)
+            console.log('error' + error)
+        }
     }
-  }
 
-  async list (companyId, jobId) {
-    try {
-    console.log('===========CompanyController      #list')
-      let where = {}
-
-      if (companyId) {
-        where.id = id
-      }
-
-      if(jobId){
-        where.jobId = jobId
-      }
-
-      const fields = ['name']
-
-      const data = await Company.findAll({
-        attributes: fields,
-        include: [
-          { model: Job, as: 'jobs', attributes: ['id','title', 'description', 'location', 'notes', 'status'] }
-        ],
-        where: where,
-        limit: 10,
-        offset: 0
-      }) 
-
-      return data
-    } catch (error) {
-        console.log('error' + error)
-        throw new Error(error)
+    async get(req,res){
+        try {
+            const companyId = req.params.company_id
+            console.log('=============Recebido: ' + companyId)
+            const companies = await this.companyRepository.get(companyId)
+            res.status(200).json(companies)
+        } catch (error) {
+            res.status(500).json({status: 'error', message: error.message})
+            console.log('error' + error)
+        }
     }
-  }
-
-  async update(id, data) {
-    try {
-    console.log('===========CompanyController      #update:'+id)
-      let where = {}
-      where.id = id
-      const result = await Company.update(data, {
-        where: where
-      })
-      return result
-    } catch (error) {
-      console.log('error' + error)
-      throw new Error(error)
-    }
-  }
-
-  async delete(id) {
-    try {
-    console.log('===========CompanyController      #delete:'+id)
-      let where = {}
-      where.id = id
-      const result = await Company.destroy({
-        where: where
-      })
-      return result
-    } catch (error) {
-      console.log('error' + error)
-      throw new Error(error)
-    }
-  }
+    
 }
 
-export default Companies
+export default CompanyController
