21e640899a70ea5051bb66e6b90057a9571d114b
amilcarpio <amilcar.pio@gmail.com>
2024-02-23 22:00:17 -0300

    Fix: fixing bugs

--- test/integration/feedController.test.js
+++ test/integration/feedController.test.js
@@ -6,9 +6,9 @@ describe('Feed', function() {
         const response = await axios.get('http://localhost:3000/api/feed');
 
         expect(response.status).to.equal(200);
-        expect(response.data).to.have.property('title').to.equal('Dev backend')
-        expect(response.data).to.have.property('description').to.equal('Develop the backend for the new app')
-        expect(response.data).to.have.property('companyName').to.equal('Google')
-        expect(response.data).to.have.property('createdAt').to.equal('2021-01-01T00:00:00')
+        expect(response.data[0]).to.have.property('title').to.equal('Dev backend')
+        expect(response.data[0]).to.have.property('description').to.equal('Develop the backend for the new app')
+        expect(response.data[0]).to.have.property('companyName').to.equal('Google')
+        expect(response.data[0]).to.have.property('createdAt').to.equal('2021-01-01T00:00:00')
     }).timeout(5000)
 });
