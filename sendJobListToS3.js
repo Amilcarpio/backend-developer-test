b752a5abca8226b59161c98ed8c58603a0aa3edb
amilcarpio <amilcar.pio@gmail.com>
2024-02-23 21:40:56 -0300

    Fix: Fixes

--- sendJobListToS3.js
+++ sendJobListToS3.js
@@ -5,13 +5,25 @@ const jobRepository = new JobRepository();
 const s3 = new AWS.S3();
 
 module.exports.handler = async (event) => {
-    const jobsList = await jobRepository.list(null, 'published', null, null, null);
-
+    const jobsList = await jobRepository.list(null, null, null, 'published', null, null, null);
+    
+    let jobsDAO = []
     if(jobsList.length === 0) {
+        console.log('=========No jobs founded')
         return {
-            statusCode: 204,
+            statusCode: 404,
             body: JSON.stringify({ message: 'No jobs found' })
         };
+    }
+
+    for (let i = 0; i < jobsList.length; i++) {
+        jobsDAO.push({
+            id: jobsList[i].dataValues.id,
+            title: jobsList[i].dataValues.title,
+            description: jobsList[i].dataValues.description,
+            companyName: jobsList[i].dataValues.company.name,
+            createdAt: jobsList[i].dataValues.createdAt
+        });
     }
 
     const params = {
